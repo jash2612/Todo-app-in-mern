1dc9f5d9f6ecb20d19e49594a29e74d096690766
jash2612 <127189426+jash2612@users.noreply.github.com>
2025-05-14 19:28:25 +0530

    Update vite.config.js

--- mern-todo-app/client/vite.config.js
+++ mern-todo-app/client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': 'http://localhost:5000'
+      '/api': 'https://todo-app-in-mern.onrender.com'
     }
   }
 });
